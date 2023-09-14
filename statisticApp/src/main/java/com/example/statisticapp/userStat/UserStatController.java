ade792c732fb1b4baeaf2c8d643125d17cfdb5d6
baklyNik <baklykov.nikita369@gmail.com>
2023-09-15 02:13:10 +0300

    add controller

---
+++ statisticApp/src/main/java/com/example/statisticapp/userStat/UserStatController.java
@@ -0,0 +1,33 @@
+package com.example.statisticapp.userStat;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/stat/users")
+@AllArgsConstructor
+public class UserStatController {
+    private final UserStatService userStatService;
+
+    @PostMapping
+    public void createUserStat(@RequestBody UserStatDto userStatDto) {
+        if (userStatDto.getActionTime() == null) {
+            userStatDto.setActionTime(Instant.now());
+        }
+        userStatService.createUserStat(userStatDto);
+    }
+
+    @GetMapping("/{userLogin}")
+    public List<UserStatDto> findUsersStatByLogin(@PathVariable String userLogin) {
+        return userStatService.findUsersStatByLogin(userLogin);
+    }
+
+    @GetMapping
+    public List<UserStatDto> findAllUsersStat() {
+        return userStatService.findAllUsersStat();
+    }
+
+}
