c405355ee0c56cf2e84176e9bc4258a714fd72f0
baklyNik <baklykov.nikita369@gmail.com>
2023-09-17 20:06:04 +0300

    add log

--- mainApp/src/main/java/com/example/mainapp/exception/ErrorHandler.java
+++ mainApp/src/main/java/com/example/mainapp/exception/ErrorHandler.java
@@ -1,5 +1,6 @@
 package com.example.mainapp.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,12 +10,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNotFound(final NotFoundException e) {
+        log.error(e.getMessage());
         return Map.of(
                 "Error", HttpStatus.NOT_FOUND.getReasonPhrase(),
                 "errorMessage", e.getMessage(),
@@ -25,6 +28,7 @@ public class ErrorHandler {
     @ExceptionHandler(InternalServerErrorException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleInternalServerError(final InternalServerErrorException e) {
+        log.error(e.getMessage());
         return Map.of(
                 "Error", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
                 "errorMessage", e.getMessage(),
